819e6255d0b9688273abf8966826b2e95ffb0940
Artur Gubaidullin <lapteuh@gmail.com>
2025-01-25 12:18:08 +0000

    Add vite config

---
+++ bundle/vite.config.ts
@@ -0,0 +1,29 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vite";
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+const url = import.meta.url;
+
+const __dirname = dirname(fileURLToPath(url));
+
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    lib: {
+      entry: {
+        index: resolve(__dirname, "src/index.ts"),
+        result: resolve(__dirname, "src/result.ts"),
+      },
+      name: "@arthurgubaidullin/lib",
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        globals: {},
+      },
+    },
+  },
+});
